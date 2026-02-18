b0ed4d1232f1ecc65dc79a264f1b5e5443e19fc3
Matthew Seales <Rebelrabbit1975@yahoo.com>
2026-02-17 20:14:27 -0500

    Delete database directory
    cleaning house

--- database/policies/user_settings.sql
+++
@@ -1,5 +0,0 @@
--- Add settings, bio, phone to users table
-alter table public.users
-add column if not exists settings jsonb default '{}'::jsonb,
-add column if not exists bio text,
-add column if not exists phone text;
